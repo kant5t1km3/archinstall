e27a519de1b44db9bbd97227a6bab898b2cb0f72
kant5t1km3 <kant5t1km3@users.noreply.github.com>
2020-06-17 06:27:07 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -292,7 +292,7 @@ net.ipv4.tcp_max_syn_backlog = 1280
 END
 
 echo "Final application setup"
-pacman -Syu --needed --noconfirm xorg-server xorg-xinit xorg-xrandr xf86-video-intel mesa libvirt ebtables dnsmasq bridge-utils virt-manager firefox zsh-completions wget curl transmission-cli tldr signal-desktop ffmpeg vlc rsync bleachbit neofetch man-db man-pages texinfo ufw clamav rkhunter util-linux tlp powertop throttled unzip unrar p7zip net-tools nmap xf86-input-libinput tree htop python go python-pip acpi whois speedtest-cli qutebrowser adb ntp strace tcpdump tcpreplay wireshark-qt clang cmake gdb
+pacman -Syu --needed --noconfirm xorg-server xorg-xinit xorg-xrandr xf86-video-intel redshift mesa libvirt ebtables dnsmasq bridge-utils virt-manager firefox zsh-completions wget curl transmission-cli tldr signal-desktop ffmpeg vlc rsync bleachbit neofetch man-db man-pages texinfo ufw clamav rkhunter util-linux tlp powertop throttled unzip unrar p7zip net-tools nmap xf86-input-libinput tree htop python go python-pip acpi whois speedtest-cli qutebrowser adb ntp strace tcpdump tcpreplay wireshark-qt clang cmake gdb
 sudo systemctl enable --now lenovo_fix.service
 yay -S slack-desktop spotify libreoffice codium-bin s-tui cava protonmail-bridge
 sudo pip3 install somafm colorama requests
