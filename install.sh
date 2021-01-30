6cc3337a5fc6792c539c90070d51802dd58b68e8
kant5t1km3 <kant5t1km3@users.noreply.github.com>
2021-01-30 15:00:53 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,14 +18,14 @@ timedatectl set-ntp true
 # pacman -Sy --noconfirm gnupg archlinux-keyring
 
 echo "Syncing packages database and Finding Mirrors"
-pacman -Sy reflector --noconfirm
-reflector -a 2 -l 100 -f 10 --sort score --save /etc/pacman.d/mirrorlist
+pacman -Sy reflector rsync --noconfirm
+reflector --country "United States" -a 2 -l 100 -f 10 --sort score --save /etc/pacman.d/mirrorlist
 
 echo "Creating partition tables"
 blkdiscard /dev/nvme0n1 -f
 printf "o\nw\ny\n" | gdisk /dev/nvme0n1
 printf "n\n1\n4096\n+512M\nef00\nw\ny\n" | gdisk /dev/nvme0n1
-printf "n\n2\n\n\n8e00\nw\ny\n" | gdisk /dev/nvme0n1
+printf "n\n2\n\n\n8300\nw\ny\n" | gdisk /dev/nvme0n1
 
 #echo "Zeroing partitions"
 #cat /dev/zero > /dev/nvme0n1p1
@@ -35,33 +35,36 @@ echo "Setting up cryptographic volume"
 modprobe dm-crypt
 modprobe dm-mod
 printf "%s" "$encryption_passphrase" | cryptsetup --use-random luksFormat /dev/nvme0n1p2
-printf "%s" "$encryption_passphrase" | cryptsetup luksOpen /dev/nvme0n1p2 cryptlvm
+printf "%s" "$encryption_passphrase" | cryptsetup luksOpen /dev/nvme0n1p2 cryptroot
 
 echo "Creating physical volume"
-pvcreate /dev/mapper/cryptlvm
+mkfs.fat -F32 -n LINUXEFI /dev/nvme0n1p1
+mkfs.btrfs -L Arch /dev/mapper/cryptroot
 
-echo "Creating volume volume"
-vgcreate vg0 /dev/mapper/cryptlvm
+mount -o compress=zstd,noatime /dev/mapper/cryptroot /mnt
+btrfs subvol create /mnt/@
+btrfs subvol create /mnt/@home
+btrfs subvol create /mnt/@swap
 
-echo "Creating logical volumes"
-lvcreate -L +"$swap_size"GB vg0 -n swap
-lvcreate -l +100%FREE vg0 -n root
+mkdir /mnt/snapshots
+btrfs subvol create /mnt/snapshots/@
+btrfs subvol create /mnt/snapshots/@home
 
-echo "Setting up / partition"
-yes | mkfs.ext4 /dev/vg0/root
-mount /dev/vg0/root /mnt
-
-echo "Setting up /boot partition"
-yes | mkfs.fat -F32 /dev/nvme0n1p1
-mkdir /mnt/boot
+umount /mnt
+mount -o compress=zstd,noatime,subvol=@ /dev/mapper/cryptroot /mnt
+mkdir -p /mnt/{boot,home}
+mount -o compress=zstd,noatime,subvol=@home /dev/mapper/cryptroot /mnt/home
 mount /dev/nvme0n1p1 /mnt/boot
 
-echo "Setting up swap"
-yes | mkswap /dev/vg0/swap
-swapon /dev/vg0/swap
+mkdir /mnt/var
+btrfs subvol create /mnt/var/cache
+btrfs subvol create /mnt/var/log
+
+mkdir -p /mnt/var/lib/{mysql,postgres,machines}
+chattr +C /mnt/var/lib/{mysql,postgres,machines}
 
 echo "Installing Arch Linux"
-yes '' | pacstrap /mnt base base-devel linux linux-headers linux-lts linux-lts-headers linux-firmware lvm2 device-mapper e2fsprogs intel-ucode cryptsetup networkmanager wget man-db man-pages nano diffutils flatpak mkinitcpio vi vim reflector dhcpcd git sudo efibootmgr xf86-video-intel dialog wpa_supplicant pigz ufw
+yes '' | pacstrap /mnt base base-devel linux linux-headers linux-lts linux-lts-headers linux-firmware device-mapper e2fsprogs intel-ucode cryptsetup networkmanager wget man-db man-pages nano diffutils flatpak mkinitcpio btrfs-progs dosfstools vi vim rsync reflector dhcpcd git sudo efibootmgr sudo zsh zsh-completions zsh-syntax-highlighting xf86-video-intel dialog wpa_supplicant pigz ufw
 
 echo "Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -93,11 +96,8 @@ echo -en "$root_password\n$root_password" | passwd
 #echo -en "$user_password\n$user_password" | passwd $user_name
 
 echo "Generating initramfs"
-sed -i 's/^HOOKS.*/HOOKS=(base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt sd-lvm2 filesystems fsck shutdown)/' /etc/mkinitcpio.conf
-sed -i 's/^MODULES.*/MODULES=(ext4 i915 lz4 lz4_compress)/' /etc/mkinitcpio.conf
-sed -i 's/^BINARIES.*/BINARIES=(fsck fsck.ext4)/' /etc/mkinitcpio.conf
-sed -i 's/#COMPRESSION="lz4"/COMPRESSION="lz4"/g' /etc/mkinitcpio.conf
-sed -i 's/#COMPRESSION_OPTIONS=()/COMPRESSION_OPTIONS=(-9)/g' /etc/mkinitcpio.conf
+sed -i 's/^HOOKS.*/HOOKS=(base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt filesystems fsck)/' /etc/mkinitcpio.conf
+sed -i 's/^MODULES.*/MODULES=(btrfs)/' /etc/mkinitcpio.conf
 
 mkinitcpio -p linux
 mkinitcpio -p linux-lts
@@ -111,7 +111,7 @@ mkdir -p /boot/loader/
 touch /boot/loader/loader.conf
 tee -a /boot/loader/loader.conf << END
 default arch.conf
-timeout 3
+timeout 2
 editor 0
 END
 
@@ -123,7 +123,7 @@ title Arch Linux LTS
 linux /vmlinuz-linux-lts
 initrd /intel-ucode.img
 initrd /initramfs-linux-lts.img
-options rd.luks.name=$drive_id=cryptlvm root=/dev/vg0/root resume=/dev/vg0/swap rd.luks.options=discard quiet loglevel=3 splash rw
+options rd.luks.name=$drive_id=cryptroot root=/dev/mapper/cryptroot rootflags=subvol=@ rd.luks.options=discard rw
 END
 
 touch /boot/loader/entries/arch.conf
@@ -132,7 +132,7 @@ title Arch Linux
 linux /vmlinuz-linux
 initrd /intel-ucode.img
 initrd /initramfs-linux.img
-options rd.luks.name=$drive_id=cryptlvm root=/dev/vg0/root resume=/dev/vg0/swap rd.luks.options=discard quiet loglevel=3 splash rw
+options rd.luks.name=$drive_id=cryptroot root=/dev/mapper/cryptroot rootflags=subvol=@ rd.luks.options=discard rw
 END
 
 echo "Setting up Pacman hook for automatic systemd-boot updates"
@@ -164,7 +164,7 @@ Target = pacman-mirrorlist
 Description = Updating pacman-mirrorlist with reflector and removing pacnew...
 When = PostTransaction
 Depends = reflector
-Exec = /bin/sh -c "reflector -a 2 -l 100 -f 10 --sort score --save /etc/pacman.d/mirrorlist; rm -f /etc/pacman.d/mirrorlist.pacnew"
+Exec = /bin/sh -c "reflector --country "United States" -a 2 -l 100 -f 10 --sort score --save /etc/pacman.d/mirrorlist; rm -f /etc/pacman.d/mirrorlist.pacnew"
 END
 
 echo "Setting up Scheduler Rules"
@@ -193,29 +193,63 @@ echo "Enabling Network Manager"
 systemctl enable NetworkManager
 
 echo "User Config"
-reflector -a 2 -l 100 -f 10 --sort score --save /etc/pacman.d/mirrorlist
-#echo '%wheel ALL=(ALL) ALL' | EDITOR='tee -a' visudo
+timedatectl set-ntp true
+reflector --country "United States" -a 2 -l 100 -f 10 --sort score --save /etc/pacman.d/mirrorlist
+useradd -m -G wheel,libvirt,kvm,users -s /usr/bin/zsh aragorn
+systemctl enable libvirtd
 
-wget https://raw.githubusercontent.com/brianclemens/dotfiles/011a080c6f5e87631623baf0aad826ff2b99566c/misc/sudoers.lecture
+echo "Setting user password"
+echo -en "$user_password\n$user_password" | passwd aragorn
+
+echo '%wheel ALL=(ALL) ALL' | EDITOR='tee -a' visudo
+systemctl enable fstrim.timer
+
+wget https://raw.githubusercontent.com/brianclemens/dotfiles/011a0rootflags=subvol=@ rd.luks.options=discard rw80c6f5e87631623baf0aad826ff2b99566c/misc/sudoers.lecture
 mv sudoers.lecture /etc/bee-sudoers.lecture
 tee -a /etc/sudoers << END
 Defaults    lecture=always
 Defaults    lecture_file=/etc/bee-sudoers.lecture
 END
 
-curl -LO larbs.xyz/larbs.sh
-sh larbs.sh
+#curl -LO larbs.xyz/larbs.sh
+#sh larbs.sh
+
+# Paru helper
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git /tmp/paru
+cd /tmp/paru
+makepkg -si
+
+# Plasma 
+sudo pacman -S xorg-server xf86-video-fbdev xf86-video-nouveau xf86-video-intel adobe-source-code-pro-fonts noto-fonts-emoji virt-manager edk2-ovmf qemu libvirt firefox colord-kde plasma-meta sddm kdialog konsole dolphin noto-fonts phonon-qt5-vlc
+
+sudo mkdir /etc/sddm.conf.d
+tee -a /etc/sddm.conf.d/kde_settings.conf << END
+[General]
+HaltCommand=/usr/bin/systemctl poweroff
+Numlock=none
+RebootCommand=/usr/bin/systemctl reboot
+
+[Theme]
+Current=breeze
+
+[Users]
+MaximumUid=60000
+MinimumUid=1000
+END
+sudo systemctl enable sddm
+
 sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -z - --threads=0)/g' /etc/makepkg.conf
 sed -i 's/COMPRESSGZ=(gzip -c -f -n)/COMPRESSGZ=(pigz -c -f -n)/g' /etc/makepkg.conf
 
-echo "Setting autologin"
-mkdir -p /etc/systemd/system/getty@tty1.service.d
-tee -a /etc/systemd/system/getty@tty1.service.d/override.conf << END
-[Service]
-Type=Simple
-ExecStart=
-ExecStart=-/usr/bin/agetty --autologin $user_name --noclear %I \$TERM
-END
+#echo "Setting autologin"
+#mkdir -p /etc/systemd/system/getty@tty1.service.d
+#tee -a /etc/systemd/system/getty@tty1.service.d/override.conf << END
+#[Service]
+#Type=Simple
+#ExecStart=
+#ExecStart=-/usr/bin/agetty --autologin $user_name --noclear %I \$TERM
+#END
 
 echo "Hardening TCP/IP stack"
 ufw default deny
@@ -277,15 +311,15 @@ net.ipv4.conf.all.secure_redirects = 1 #CentOS Wiki says 0 here.
 net.ipv4.tcp_max_syn_backlog = 1280
 END
 
-echo "Final application setup"
-wget https://raw.githubusercontent.com/kant5t1km3/archinstall/master/pkglist 
-pacman -Syu --noconfirm - < pkglist
+#echo "Final application setup"
+#wget https://raw.githubusercontent.com/kant5t1km3/archinstall/master/pkglist 
+#pacman -Syu --noconfirm - < pkglist
 #sudo systemctl enable --now lenovo_fix.service
 
 # Exit arch-chroot
 EOF
 
-umount -R /mnt
-swapoff -a
+#umount -R /mnt
+#swapoff -a
 
 echo "Arch Linux is ready. You can reboot now!"
